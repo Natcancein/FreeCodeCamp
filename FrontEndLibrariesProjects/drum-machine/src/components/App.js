2a0a3950ab765e9be0f5edc30bb882b53b35d5af
natcancein <nataliaconstanz@gmail.com>
2019-04-21 15:29:44 -0400

    changing styles drum-machine

--- FrontEndLibrariesProjects/drum-machine/src/components/App.js
+++ FrontEndLibrariesProjects/drum-machine/src/components/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
 import '../styles/App.css';
 import styled from 'styled-components';
-
+import Display from './Display';
 // Wrapper with styled component 
 
 const Wrapper = styled.section`
- 
-  padding: 4em;
-  background: papayawhip;
-  max-width: 50%;
- 
+  padding-top: 2em;
+  padding-bottom:3em;
+  background: papayawhip; 
 `;
 
 class App extends Component {
     render() {
         return (
-            <div className="app">
+            <div className="app" id="drum-machine">
         
                 <Wrapper>
   
                 <h1>Drum Machine</h1>
+                <Display/>
 
   </Wrapper>
             </div>
---
+++ FrontEndLibrariesProjects/drum-machine/src/components/Display.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Display = () => (
+    <div  className="display">
+    <div className="drum-pad" id="Q">Q</div>
+    <div className="drum-pad" id="W">W</div>
+    <div className="drum-pad" id="E">E</div>
+    <div className="drum-pad" id="A">A</div>
+    <div className="drum-pad" id="S">S</div>
+    <div className="drum-pad" id="D">D</div>
+    <div className="drum-pad" id="Z">Z</div>
+    <div className="drum-pad" id="X">X</div>
+    <div className="drum-pad" id="C">C</div>
+    </div>
+   
+);
+
+export default Display;
